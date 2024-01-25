98be61e036583be02549e1e567c7192e6fc9d56c
AleAlpizar <133933772+AleAlpizar@users.noreply.github.com>
2024-01-24 18:55:44 -0600

    Creacion de proyecto

---
+++ Tienda_AlexandraAlpizar/src/main/java/Tienda/demo/TiendaAlexandraAlpizarApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAlexandraAlpizarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAlexandraAlpizarApplication.class, args);
+	}
+
+}
