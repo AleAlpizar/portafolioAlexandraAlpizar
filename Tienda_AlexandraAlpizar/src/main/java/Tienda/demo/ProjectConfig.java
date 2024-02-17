461333f6ea825a541e3f87f0758f69e00322ec43
AleAlpizar <133933772+AleAlpizar@users.noreply.github.com>
2024-02-16 23:42:52 -0600

    semana 5

--- Tienda_AlexandraAlpizar/src/main/java/Tienda/demo/ProjectConfig.java
+++ Tienda_AlexandraAlpizar/src/main/java/Tienda/demo/ProjectConfig.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pp1;
+package Tienda.demo;
 
 /**
  *
  * @author 11alp
  */
-public class Ejercicios {
-    static void tablas 
+public class ProjectConfig {
+    
 }
