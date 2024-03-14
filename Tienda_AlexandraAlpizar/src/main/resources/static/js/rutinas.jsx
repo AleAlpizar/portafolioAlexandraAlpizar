1934308d10a3dd699b62cb3eb7a1bd4bee7e6dfa
AleAlpizar <133933772+AleAlpizar@users.noreply.github.com>
2024-03-13 23:42:35 -0600

    Semana 8(se agrego unas cosas que faltaban)

--- Tienda_AlexandraAlpizar/src/main/resources/static/js/rutinas.jsx
+++ Tienda_AlexandraAlpizar/src/main/resources/static/js/rutinas.jsx
@@ -1,11 +1,22 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/reactjs.jsx to edit this template
- */
-var Hello = React.createClass({
-    render: function () {
-        return (
-                <div></div>
-                );
+/* La siguiente función se utiliza para visualizar la imagen seleccionada en la
+ * página html donde se desea "cargar" utilizando un llamado "ajax"*/
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#blah')
+                    .attr('src', e.target.result)
+                    .height(200);
+        };
+        reader.readAsDataURL(input.files[0]);
     }
-});
+}
+
+/* La siguiente función se utiliza para activar la cantidad de elementos seleccionados
+ * En el carrito de compras utilizando un llamado "ajax" */
+function addCard(formulario) {
+    var valor = formulario.elements[0].value;
+    var url = '/carrito/agregar';
+    url = url + '/' + valor;
+    $("#resultsBlock").load(url);
+}
